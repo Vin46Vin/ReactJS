8199ce7cfce20e45d6a29bb7686cac481a015c85
Vin46vin <kyourin46@gmail.com>
2024-05-03 16:23:43 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+const App   = () => {
+    return ( 
+    <div ClassName = "app">
+        <h1> My Name is VIN</h1>
+        <p>
+            I am a student of Built Bright University
+        </p> 
+            
+    </div>
+    );
+
+};
+
+export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import "./Card.css"
+
+const Card = () => {
+    //const style = {
+       // color: red,
+   // };
+  return (
+
+    <>
+        <div className="card">this is card</div>
+        <button className="btn">Click Me</button>
+    
+    </>
+  );
+};
+
+export default Card
